98871bf0bba15b671a05f86ed256dbc004f70be0
naattmoo <natmo21@gmail.com>
2020-08-11 13:19:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,  request
+import json
 
 
 app = Flask(__name__)
@@ -8,7 +9,7 @@ app = Flask(__name__)
 def sof():
     productName = request.args.get('productName')
     result=[{"product":"Policy Enforcement vRAS","attributes":["Number of Users"]},{"product":"Virtual Remote Access Service","attributes":["Submitter Emails","Operational Contact","Service Center Region","Service Center Location","NFH Point of Presence","Service Contract Term","Customer VDOM Name","Customer VDOM Short Name","Syslog Server IP Address","Interlink IP Address - Customer VDOM side","Interlink IP Address - Inet VDOM side","Network Mask","Customer MPLS VPN","Customer's Private IP Prefix","Private IP Prefix Addresing","Delegate's Private IP Prefix","VPN SSL Users","VPN IPSec Users","Traffic Shapping","BW User","IP range start of users VPN SSL","Ip range end of users VPN SSL","Ip range start of users VPN IPSec","Ip range end of users VPN IPSec","Designated Fortinet Cluster","Name of Interlink to Inet VDOM","RAS Tunnel Termination Public IP Address","SSL certificate"]}]
-    return result
+    return json.dumps(result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
